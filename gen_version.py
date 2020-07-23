7e3f2c2f5099490e1b38ce97ce161ae8ae79c9e8
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-07-23 22:40:17 +0200

    Use tilde instead of dash for dev versions
    RPM doesn't support dash in version numbers. From
    http://ftp.rpm.org/max-rpm/ch-rpm-file-format.html: 'The only
    restriction placed on the version is that it cannot contain a dash
    "-".'
    
    See also https://github.com/semver/semver/issues/145.

--- gen_version.py
+++ gen_version.py
@@ -1,24 +1,24 @@
 # Generate version
 #
 # Stable release entry format     : <major>.<minor>.>patch>
-# Development release entry format: <major>.<minor>.<patch>-dev
+# Development release entry format: <major>.<minor>.<patch>~dev
 #
 # Stable release output format     : <major>.<minor>.<patch>
-# Development release output format: <major>.<minor>.<patch>-dev.<YYYYmmdd.HHMM>
+# Development release output format: <major>.<minor>.<patch>~dev.<YYYYmmdd.HHMM>
 
 # The patch version of a development release should be the next stable
-# release patch number followed by "-dev". The committer date will be
+# release patch number followed by "~dev". The committer date will be
 # added with a period separator.
 #
 # Example sequence:
 #
 # Entry          Output
 # 2.44.1      => 2.44.1
-# 2.44.2-dev  => 2.44.2-dev.20200704.1652
+# 2.44.2~dev  => 2.44.2~dev.20200704.1652
 # 2.44.2      => 2.44.2
-# 2.44.3-dev  => 2.44.3-dev.20200824.1337
+# 2.44.3~dev  => 2.44.3~dev.20200824.1337
 
-version = '2.44.2-dev'
+version = '2.44.2~dev'
 
 import os
 import sys
