ffc2e8d391a67b1d56459dd7278aa7b96c89e828
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-07-27 12:46:52 +0200

    Make test_installation.py self-contained to run also on Windows
    Also removed the now obsolete check_installation.sh bash script.

--- tests/regression_tests/installation/test_installation.py
+++ tests/regression_tests/installation/test_installation.py
@@ -1,9 +1,19 @@
+import pytest
 import subprocess
 import os
 
 def test_installation():
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    result = subprocess.Popen(['bash', './check_installation.sh'])
-    text = result.communicate()[0]
-    print(text)
-    assert result.returncode == 0
+    expected_version = os.environ['GV_VERSION']
+    actual_version_string = subprocess.check_output(
+        [
+            'dot',
+            '-V',
+        ],
+        universal_newlines=True,
+        stderr=subprocess.STDOUT,
+    )
+    try:
+        actual_version = actual_version_string.split()[4]
+    except IndexError:
+        pytest.fail('Malformed version string: {0}'.format(actual_version_string))
+    assert actual_version == expected_version
