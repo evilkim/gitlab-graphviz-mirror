c44c9777c798dcaa77e9600b542b6272c0711e23
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-08-16 10:51:02 +0200

    Add windows/bin/setup-and-build.ps1

---
+++ windows/bin/setup-and-build.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = "Stop"
+
+$dir = $PSScriptRoot
+
+Invoke-Expression "$dir\setup-build-utilities.ps1"
+
+powershell.exe -ExecutionPolicy Bypass -Command "$dir\build.ps1" $args
