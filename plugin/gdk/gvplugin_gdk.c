729bc9e99906cd92697c2b49461371a1dd943198
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-08-28 18:54:19 -0700

    disambiguate plugin/gdk #includes
    Related to #1785.

--- plugin/gdk/gvplugin_gdk.c
+++ plugin/gdk/gvplugin_gdk.c
@@ -11,7 +11,7 @@
  * Contributors: See CVS logs. Details at http://www.graphviz.org/
  *************************************************************************/
 
-#include "gvplugin.h"
+#include <gvc/gvplugin.h>
 
 extern gvplugin_installed_t gvdevice_gdk_types[];
 extern gvplugin_installed_t gvloadimage_gdk_types[];
