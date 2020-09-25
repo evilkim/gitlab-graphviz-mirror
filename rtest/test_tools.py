53211c003b6ed7701c27e07316ed6b651642643b
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-09-25 16:57:05 +0200

    Skip execution of all tools not built with CMake in tools test for CMake builds
    Fails because not all tools are built with CMake. See
    https://gitlab.com/graphviz/graphviz/-/issues/1753 and
    https://gitlab.com/graphviz/graphviz/-/issues/1836.

--- rtest/test_tools.py
+++ rtest/test_tools.py
@@ -34,6 +34,21 @@ import subprocess
 ])
 def test_tools(tool):
 
+    tools_not_built_with_cmake = [
+        'diffimg',
+        'dotty',
+        'edgepaint',
+        'gc',
+        'gv2gml',
+        'gvmap',
+        'gvpr',
+        'lefty',
+        'lneato',
+        'mingle',
+        'prune',
+        'smyrna',
+    ]
+
     os_id = os.getenv('OS_ID')
 
     # FIXME: Remove skip when
@@ -50,6 +65,13 @@ def test_tools(tool):
     # https://gitlab.com/graphviz/graphviz/-/issues/1835 is fixed
     if tool == 'mingle' and os_id in ['ubuntu', 'centos']:
       pytest.skip('mingle is not built for ' + os_id + ' (#1835)')
+
+    # FIXME: Remove skip when
+    # https://gitlab.com/graphviz/graphviz/-/issues/1753 and
+    # https://gitlab.com/graphviz/graphviz/-/issues/1836 is fixed
+    if os.getenv('build_system') == 'cmake':
+      if tool in tools_not_built_with_cmake:
+        pytest.skip(tool + ' is not built with CMake (#1753 & #1836)')
 
     subprocess.check_call(
         [tool, '-?'],
