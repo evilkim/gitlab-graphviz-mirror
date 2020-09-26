df2cbb3b512d26b883b4a96ae3535cb15177a6ca
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-09-26 13:26:06 +0200

    Fix gvmap.sh -? option to exit with zero exit status

--- cmd/gvmap/gvmap.sh
+++ cmd/gvmap/gvmap.sh
@@ -66,7 +66,7 @@ do
   \? )
     if [[ "$OPTARG" == '?' ]]
     then
-      getopts -a gvmap "$OPTSTR" x '-?'
+      (getopts -a gvmap "$OPTSTR" x '-?')
       exit 0
     else
       print -u 2 "gvmap: unknown flag $OPTARG - ignored"
