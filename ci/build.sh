4f6428dbd7576815db870c362a605d9068763b6c
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-09-30 16:50:52 +0200

    Refactor ci/build.sh to check for build system before OS

--- ci/build.sh
+++ ci/build.sh
@@ -16,36 +16,38 @@ GV_VERSION=$( cat VERSION )
 COLLECTION=$( cat COLLECTION )
 META_DATA_DIR=Metadata/${COLLECTION}/${ID}/${VERSION_ID}
 mkdir -p ${META_DATA_DIR}
-if [ "${ID_LIKE}" = "debian" ]; then
-    if [ "${build_system}" = "cmake" ]; then
-        mkdir build
-        cd build
-        cmake ..
-        cmake --build .
-        cpack
-        cd ..
-    else
+if [ "${build_system}" = "cmake" ]; then
+    mkdir build
+    cd build
+    cmake ..
+    cmake --build .
+    cpack
+    cd ..
+else
+    if [ "${ID_LIKE}" = "debian" ]; then
         tar xfz graphviz-${GV_VERSION}.tar.gz
         (cd graphviz-${GV_VERSION}; fakeroot make -f debian/rules binary) | tee >(ci/extract-configure-log.sh >${META_DATA_DIR}/configure.log)
+    else
+        rm -rf ${HOME}/rpmbuild
+        rpmbuild -ta graphviz-${GV_VERSION}.tar.gz | tee >(ci/extract-configure-log.sh >${META_DATA_DIR}/configure.log)
     fi
-else
-    rm -rf ${HOME}/rpmbuild
-    rpmbuild -ta graphviz-${GV_VERSION}.tar.gz | tee >(ci/extract-configure-log.sh >${META_DATA_DIR}/configure.log)
 fi
 DIR=Packages/${COLLECTION}/${ID}/${VERSION_ID}
 ARCH=$( uname -m )
 mkdir -p ${DIR}/os/${ARCH}
 mkdir -p ${DIR}/debug/${ARCH}
 mkdir -p ${DIR}/source
-if [ "${ID_LIKE}" = "debian" ]; then
-    if [ "${build_system}" = "cmake" ]; then
+if [ "${build_system}" = "cmake" ]; then
+    if [ "${ID_LIKE}" = "debian" ]; then
         mv build/*.deb ${DIR}/os/${ARCH}/
-    else
-        mv *.deb ${DIR}/os/${ARCH}/
-        mv *.ddeb ${DIR}/debug/${ARCH}/
     fi
 else
-    mv ${HOME}/rpmbuild/SRPMS/*.src.rpm ${DIR}/source/
-    mv ${HOME}/rpmbuild/RPMS/*/*debuginfo*rpm ${DIR}/debug/${ARCH}/
-    mv ${HOME}/rpmbuild/RPMS/*/*.rpm ${DIR}/os/${ARCH}/
+    if [ "${ID_LIKE}" = "debian" ]; then
+        mv *.deb ${DIR}/os/${ARCH}/
+        mv *.ddeb ${DIR}/debug/${ARCH}/
+    else
+        mv ${HOME}/rpmbuild/SRPMS/*.src.rpm ${DIR}/source/
+        mv ${HOME}/rpmbuild/RPMS/*/*debuginfo*rpm ${DIR}/debug/${ARCH}/
+        mv ${HOME}/rpmbuild/RPMS/*/*.rpm ${DIR}/os/${ARCH}/
+    fi
 fi
