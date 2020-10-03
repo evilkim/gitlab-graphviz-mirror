49555c4c2695fa953ee1a99421ee565a749a186e
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-10-03 16:15:01 +0200

    Explicitly specify -? as an option in tred2

--- cmd/tools/tred2.c
+++ cmd/tools/tred2.c
@@ -60,10 +60,10 @@ static void init(int argc, char *argv[])
 
     CmdName = argv[0];
     opterr = 0;
-    while ((c = getopt(argc, argv, ":")) != -1) {
+    while ((c = getopt(argc, argv, ":?")) != -1) {
 	switch (c) {
 	case '?':
-	    if (optopt == '?')
+	    if (optopt == '\0')
 		usage(0);
 	    else {
 		fprintf(stderr, "%s: option -%c unrecognized\n",
