193dc14edbaa2b6a243754aec53939aa6aeb7cfd
Magnus Jacobsson <magnus.jacobsson@berotec.se>
2020-10-04 20:50:53 +0200

    Explicitly specify -? as an option in gc

--- cmd/tools/gc.c
+++ cmd/tools/gc.c
@@ -89,7 +89,7 @@ static void init(int argc, char *argv[])
     unsigned int c;
 
     opterr = 0;
-    while ((c = getopt(argc, argv, "necCaDUrsv")) != -1) {
+    while ((c = getopt(argc, argv, "necCaDUrsv?")) != -1) {
 	switch (c) {
 	case 'e':
 	    flags |= EDGES;
@@ -123,7 +123,7 @@ static void init(int argc, char *argv[])
 	    gtype = UNDIRECTED;
 	    break;
 	case '?':
-	    if (optopt == '?')
+	    if (optopt == '\0')
 		usage(0);
 	    else {
 		fprintf(stderr, "gc: option -%c unrecognized\n",
