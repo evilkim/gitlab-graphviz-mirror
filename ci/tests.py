52d1b4536527cb413a183dda27298e43c72922f6
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-10-24 19:34:08 -0700

    add a home for test cases that are only relevant to run in CI
    Related to #1851.

---
+++ ci/tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+test cases that are only relevant to run in CI
+'''
+
+def test_stub():
+  '''
+  a no-op test case to allow pytest to accept this file at first
+  '''
+  pass
