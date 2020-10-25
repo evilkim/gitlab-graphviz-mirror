3b17b0963bbaeb488b5975a116d01d77234cebe0
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-10-24 20:10:09 -0700

    add CMake exemptions to CI tool check
    Related to #1851.

--- ci/tests.py
+++ ci/tests.py
@@ -19,6 +19,35 @@ def test_existence(binary: str):
   check that a given binary was built and is on $PATH
   '''
 
+  tools_not_built_with_cmake = [
+    'circo',
+    'cluster',
+    'diffimg',
+    'dot2gxl',
+    'dot_builtins',
+    'dotty',
+    'edgepaint',
+    'fdp',
+    'gv2gml',
+    'gv2gxl',
+    'gvedit',
+    'gvmap',
+    'gvmap.sh',
+    'gvpr',
+    'gxl2dot',
+    'lefty',
+    'lneato',
+    'mingle',
+    'neato',
+    'osage',
+    'patchwork',
+    'prune',
+    'sfdp',
+    'smyrna',
+    'twopi',
+    'vimdot',
+  ]
+
   os_id = os.getenv('OS_ID')
 
   # FIXME: Remove skip when
@@ -38,6 +67,14 @@ def test_existence(binary: str):
   if os_id == 'centos' and binary == 'smyrna':
     check_that_tool_does_not_exist(binary, os_id)
     pytest.skip('smyrna is not built for Centos (#1834)')
+
+  # FIXME: Remove skip when
+  # https://gitlab.com/graphviz/graphviz/-/issues/1753 and
+  # https://gitlab.com/graphviz/graphviz/-/issues/1836 is fixed
+  if os.getenv('build_system') == 'cmake':
+    if binary in tools_not_built_with_cmake:
+      check_that_tool_does_not_exist(binary, os_id)
+      pytest.skip(binary + ' is not built with CMake (#1753 & #1836)')
 
   assert shutil.which(binary) is not None
 
