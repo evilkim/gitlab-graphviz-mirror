0968bc3c4158651a69a159113ed175fbc7d5d952
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-11-07 14:25:12 +0100

    Remove 6 -Wunused-but-set-variable warnings in Multilevel.c

--- lib/sfdpgen/Multilevel.c
+++ lib/sfdpgen/Multilevel.c
@@ -85,6 +85,7 @@ void Multilevel_delete(Multilevel grid){
 
 static void maximal_independent_vertex_set(SparseMatrix A, int randomize, int **vset, int *nvset, int *nzc){
   int i, ii, j, *ia, *ja, m, n, *p = NULL;
+  NOTUSED(n);
   assert(A);
   assert(SparseMatrix_known_strucural_symmetric(A));
   ia = A->ia;
@@ -137,6 +138,8 @@ static void maximal_independent_vertex_set_RS(SparseMatrix A, int randomize, int
    */
   int i, jj, ii, *p = NULL, j, k, *ia, *ja, m, n, gain, removed, nf = 0;
   PriorityQueue q;
+  NOTUSED(removed);
+  NOTUSED(n);
   assert(A);
   assert(SparseMatrix_known_strucural_symmetric(A));
 
@@ -325,6 +328,7 @@ static void maximal_independent_edge_set_heavest_edge_pernode(SparseMatrix A, in
 
 static void maximal_independent_edge_set_heavest_edge_pernode_leaves_first(SparseMatrix A, int randomize, int **cluster, int **clusterp, int *ncluster){
   int i, ii, j, *ia, *ja, m, n, *p = NULL, q;
+  NOTUSED(n);
   real *a, amax = 0;
   int first = TRUE, jamax = 0;
   int *matched, nz, ncmax = 0, nz0, nzz,k ;
@@ -500,6 +504,7 @@ static void maximal_independent_edge_set_heavest_edge_pernode_leaves_first(Spars
 
 static void maximal_independent_edge_set_heavest_edge_pernode_supernodes_first(SparseMatrix A, int randomize, int **cluster, int **clusterp, int *ncluster){
   int i, ii, j, *ia, *ja, m, n, *p = NULL;
+  NOTUSED(n);
   real *a, amax = 0;
   int first = TRUE, jamax = 0;
   int *matched, nz, nz0;
@@ -644,6 +649,7 @@ static int scomp(const void *s1, const void *s2){
 static void maximal_independent_edge_set_heavest_cluster_pernode_leaves_first(SparseMatrix A, int csize, 
 									      int randomize, int **cluster, int **clusterp, int *ncluster){
   int i, ii, j, *ia, *ja, m, n, *p = NULL, q, iv;
+  NOTUSED(n);
   real *a;
   int *matched, nz,  nz0, nzz,k, nv;
   enum {UNMATCHED = -2, MATCHED = -1};
