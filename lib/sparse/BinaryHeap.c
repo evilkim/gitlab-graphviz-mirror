6ac0762465915df7712b7c2ccfda54913241adf9
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2020-11-09 20:12:46 +0100

    Correct spelling in comment

--- lib/sparse/BinaryHeap.c
+++ lib/sparse/BinaryHeap.c
@@ -265,7 +265,7 @@ void BinaryHeap_sanity_check(BinaryHeap h){
     assert(id_to_pos[pos_to_id[i]] == i);
   }
 
-  /* all IDs, spare or in use, are ccounted for and give a contiguous set */
+  /* all IDs, spare or in use, are accounted for and give a contiguous set */
   for (i = 0; i < h->len + IntStack_get_length(h->id_stack); i++) assert(mask[i] != -1);
 
   FREE(mask);
