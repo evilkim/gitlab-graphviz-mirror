abf5666a251aaaf8d403b9fa431cacbd948c4181
Rob Hart <robhart@google.com>
2020-12-16 13:50:19 -0800

    Check for empty strings in tp and hp.
    Edit by Matthew Fernandez: Squashed a fixup commit into this one, adjusted the
    (now prior) test case to expect to pass, and added a changelog entry. For
    reference, the bug being fixed was introduced in
    31bab037c9bfde3bd18e06b5ab878c09de265ccf.
    
    Fixes #1907.

--- rtest/test_regression.py
+++ rtest/test_regression.py
@@ -559,7 +559,6 @@ def test_1869(variant: int):
     assert 'style=dashed' in output, 'style=dashed not found in DOT output'
     assert 'penwidth=2' in output, 'penwidth=2 not found in DOT output'
 
-@pytest.mark.xfail(strict=True) # FIXME
 @pytest.mark.skipif(shutil.which('twopi') is None, reason='twopi not available')
 def test_1907():
     '''
