074eeea616f3d49e0e5d74b1d4e249db5c386660
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-01-31 19:32:56 -0800

    delete no longer used strton

--- lib/ast/ast.h
+++ lib/ast/ast.h
@@ -96,7 +96,6 @@ extern "C" {
     extern int strmatch(const char *, const char *);
     extern int strgrpmatch(const char *, const char *, int *, int, int);
     extern int stresc(char *);
-    extern long strton(const char *, char **, char *, int);
     extern char *strcopy(char *s, const char *t);
 
 #endif
