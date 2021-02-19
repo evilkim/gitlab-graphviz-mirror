a93fd6c0c81ec86a200da58808d6e9f6c8bb4374
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-02-19 07:58:57 -0800

    fix multiple definitions of gvrender variables
    Similar to preceding commits, this fixes an issue where MinGW was seeing
     __declspec alternatives but implementing GCC semantics and hence treating
    declarations as definitions.

--- plugin/gd/gvrender_gd.c
+++ plugin/gd/gvrender_gd.c
@@ -288,7 +288,7 @@ static void gdgen_missingfont(char *err, char *fontreq)
 #define FONTSIZE_TOO_SMALL 1.5
 
 #ifdef _WIN32
-#   define GD_IMPORT __declspec(dllimport)
+#   define GD_IMPORT __declspec(dllimport) extern
 #else
 #   define GD_IMPORT extern
 #endif
