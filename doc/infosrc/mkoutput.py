7d5a4f3cec828227d1e88a53a0a2d06235426204
Mark Hansen <markhansen@google.com>
2021-02-28 20:51:14 +1100

    Adapt mkoutput.py to generate YAML for jekyll
    This is towards #1962. The output yaml-front-matter HTML can be used as input to jekyll.

--- doc/infosrc/mkoutput.py
+++ doc/infosrc/mkoutput.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-# Takes `outputs` as stdin and generates output.html
-# Uses `templates/output.html.j2`
-# See `outputs` file for format documentation.
 
-import markupsafe
 import re
 import sys
-from typing import Dict, Tuple
-import templates
+from typing import List
+from dataclasses import dataclass, asdict
+import yaml
+
+@dataclass
+class Output:
+    # List of command-line-params for an output format, e.g. ('jpg', 'jpeg', 'jpe')
+    params: List[str]
+    # Full name of the output format
+    name: str
+    # HTML description string
+    html_description: str
 
 HEADER_RE = re.compile(r'^:(?P<params>[^:]+):(?P<format>.*)')
 
-# Tuple of command-line-params for an output format, e.g. ('jpg', 'jpeg', 'jpe')
-params : Tuple[str, ...] = ()
-
-# Map from tuple of command-line-params to full name of the output format
-formats : Dict[Tuple[str, ...], str] = {}
-
-# Map from tuple of command-line-params to an HTML description string
-html_descriptions : Dict[Tuple[str, ...], str]  = {}
+outputs : List[Output] = []
 
 for line in sys.stdin:
     # Skip comment lines.
@@ -28,27 +27,28 @@ for line in sys.stdin:
     m = HEADER_RE.match(line)
     if m:
         # This is a header line. Grab out the values.
+        outputs.append(Output(
+            # Command-line formats are slash-separated.
+            params = m.group('params').split('/'),
+            # Full format name is plain text
+            name = m.group('format'),
+            # Set an empty string html description, ready to append to.
+            html_description = '',
+        ))
 
-        # Command-line formats are slash-separated.
-        params = tuple(m.group('params').split('/'))
-
-        # Full format name is plain text
-        formats[params] = m.group('format')
-
-        # Set an empty string html description, ready to append to.
-        html_descriptions[params] = ''
     else:
         # This is an HTML line, possibly a continuation of a previous HTML line.
-        html_descriptions[params] += line
+        outputs[-1].html_description += line
 
 
-template = templates.env().get_template('output.html.j2')
-print(template.render(
-    formats=formats,
-    # Vouch for the HTML descriptions as being safe and not needing auto-HTML-escaping.
-    # This is reasonable because the HTML descriptions are not attacker-controlled.
-    descriptions={
-        params: markupsafe.Markup(desc)
-        for params, desc in html_descriptions.items()
-    }
-))
+for o in outputs:
+    filename = o.params[0]
+    with open("_formats/" + filename + ".html", 'w') as f:
+        html_description = o.html_description
+        d = asdict(o)
+        del d["html_description"]
+
+        print("---", file=f)
+        print(yaml.dump(d), end='', file=f)
+        print("---", file=f)
+        print(html_description, end='', file=f)
