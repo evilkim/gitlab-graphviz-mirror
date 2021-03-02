2d580d0a9e936b545a0436fc585b6bc20da3291a
Mark Hansen <markhansen@google.com>
2021-03-02 21:09:20 +1100

    Generate shapes responsively: reflow small screens
    Don't use a fixed table of width 4, reflow to the available space.
    
    This was a bit of a pain to get the CSS right. This stackoverflow answer
    was very helpful: https://stackoverflow.com/a/14155983/171898
    
    This is towards moving shapes.html into graphviz.gitlab.io repo. Simpler
    for loops that jekyll can handle.

--- doc/infosrc/mkshhtml.py
+++ doc/infosrc/mkshhtml.py
@@ -7,22 +7,9 @@ import markupsafe
 import sys
 import templates
 
-N_PER_ROW = 4
-
-shapes = [line.strip() for line in sys.stdin]
-
-# From https://stackoverflow.com/a/312464/171898
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-# Use the shapes name in shape list to create the
-# contents of an HTML array.
-
 template = templates.env().get_template('shapes.html.j2')
 print(template.render(
     html_grammar=markupsafe.Markup(open(sys.argv[1]).read()),
     html1_dot=markupsafe.Markup(open(sys.argv[2]).read()),
-    rows=chunks(shapes, N_PER_ROW),
+    shapes=[line.strip() for line in sys.stdin],
 ))
