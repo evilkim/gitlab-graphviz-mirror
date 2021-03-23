24c7052635c8b5194aaea57d016aa244f1aeb418
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-03-22 20:33:59 -0700

    remove C++ alternative in dijkstra.h
    Only the C versions of these functions are implemented.

--- lib/neatogen/dijkstra.h
+++ lib/neatogen/dijkstra.h
@@ -20,22 +20,12 @@ extern "C" {
 #include <neatogen/defs.h>
 #include <neatogen/sgd.h>
 
-#ifdef __cplusplus
-    void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist);
-
-/* Dijkstra bounded to nodes in *unweighted* radius */
-    void dijkstra_bounded(int vertex, vtx_data * graph, int n,
-			  DistType * dist, int bound, int *visited_nodes,
-			  int &num_visited_nodes);
-
-#else
     extern void dijkstra(int, vtx_data *, int, DistType *);
     extern void dijkstra_f(int, vtx_data *, int, float *);
 
     /* Dijkstra bounded to nodes in *unweighted* radius */
     extern int dijkstra_bounded(int, vtx_data *, int, DistType *, int,
 				int *);
-#endif
     extern int dijkstra_sgd(graph_sgd *, int, term_sgd *);
 
 #endif
