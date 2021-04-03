3e021f9383442a4d91f76984ff286f7b0fa00633
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-04-04 00:10:58 +0200

    add test of GVContext construction without built-in plugins
    Destroy is also implicitly tested since we would detect potential
    memory leaks with ASan.
    
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2001.

--- tests/test_GVContext_construction.cpp
+++ tests/test_GVContext_construction.cpp
@@ -4,6 +4,12 @@
 
 #include <gvc++/GVContext.h>
 
+TEST_CASE("GVContext can be constructed without built-in plugins and its "
+          "underlying C data structure can be retrieved") {
+  GVC::GVContext gvc;
+  REQUIRE(gvc.c_struct() != nullptr);
+}
+
 TEST_CASE("GVContext can be constructed with built-in plugins and its "
           "underlying C data structure can be retrieved") {
   const auto demand_loading = false;
