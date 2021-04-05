4b20669907db1b70fe17e27ec29750ae1337e164
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-04-05 21:10:51 +0200

    add test of GVLayout render creating a GVRenderData object
    GVRenderData destroy is also implicitly tested since we would detect
    potential memory leaks with ASan.
    
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2001.

---
+++ tests/test_GVLayout_render.cpp
@@ -0,0 +1,26 @@
+#include <cstring>
+
+#include <catch2/catch.hpp>
+
+#include <cgraph++/AGraph.h>
+#include <gvc++/GVContext.h>
+#include <gvc++/GVLayout.h>
+#include <gvc++/GVRenderData.h>
+
+TEST_CASE("Rendered SVG can be retrieved as a string_view or as a C string") {
+  const auto demand_loading = false;
+  auto gvc =
+      std::make_shared<GVC::GVContext>(lt_preloaded_symbols, demand_loading);
+
+  auto dot = "digraph {a}";
+  auto g = std::make_shared<CGraph::AGraph>(dot);
+
+  const auto layout = GVC::GVLayout(gvc, g, "dot");
+
+  const auto result = layout.render("svg");
+  // just check that we seem to have gotten the correct format
+  REQUIRE(result.string_view().find("<!DOCTYPE svg") != std::string_view::npos);
+  REQUIRE(result.string_view().compare(result.c_str()) == 0);
+  REQUIRE(result.string_view().length() == result.length());
+  REQUIRE(std::strlen(result.c_str()) == result.length());
+}
