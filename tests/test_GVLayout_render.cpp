53348793b6be575a0612ec3b0423984c804d7728
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-04-05 21:10:51 +0200

    add test of GVLayout render throwing an exception for unknown formats
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2001.

--- tests/test_GVLayout_render.cpp
+++ tests/test_GVLayout_render.cpp
@@ -24,3 +24,16 @@ TEST_CASE("Rendered SVG can be retrieved as a string_view or as a C string") {
   REQUIRE(result.string_view().length() == result.length());
   REQUIRE(std::strlen(result.c_str()) == result.length());
 }
+
+TEST_CASE("Rendering in an unknown format throws an exception") {
+  const auto demand_loading = false;
+  auto gvc =
+      std::make_shared<GVC::GVContext>(lt_preloaded_symbols, demand_loading);
+
+  auto dot = "digraph {a}";
+  auto g = std::make_shared<CGraph::AGraph>(dot);
+
+  const auto layout = GVC::GVLayout(gvc, g, "dot");
+
+  REQUIRE_THROWS_AS(layout.render("UNKNOWN_FORMAT"), std::runtime_error);
+}
