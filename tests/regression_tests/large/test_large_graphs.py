8235d710fb7b56bdd06d6afd98afb96fe3b82238
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-04-24 10:21:51 -0700

    replace tabs in one Python file

--- tests/regression_tests/large/test_large_graphs.py
+++ tests/regression_tests/large/test_large_graphs.py
@@ -4,30 +4,30 @@ import subprocess
 import pytest
 
 class TestLargeGraphs():
-	@pytest.mark.skipif(platform.system() == "Windows" and
+  @pytest.mark.skipif(platform.system() == "Windows" and
                       platform.architecture()[0] == "32bit",
                       reason="https://gitlab.com/graphviz/graphviz/-/issues/1710")
-	def test_long_chain(self):
-		"""
-		This test will fail on 32bit Windows machine if compiled with stack size < 16MB.
-		long_chain input file generated using code below:
-			from graphviz import Digraph
+  def test_long_chain(self):
+    """
+    This test will fail on 32bit Windows machine if compiled with stack size < 16MB.
+    long_chain input file generated using code below:
+      from graphviz import Digraph
 
-			graph = Digraph(format="svg")
+      graph = Digraph(format="svg")
 
-			prev = "start"
-			graph.node("start", label="start")
+      prev = "start"
+      graph.node("start", label="start")
 
-			for i in range(33000):
-				new_node = str(i)
-				graph.node(new_node, label=new_node, shape="rectangle")
+      for i in range(33000):
+        new_node = str(i)
+        graph.node(new_node, label=new_node, shape="rectangle")
 
-				graph.edge(prev, new_node)
+        graph.edge(prev, new_node)
 
-				prev = new_node
+        prev = new_node
 
-			graph.render("long_chain")
-		"""
-		subprocess.check_call([
-			"dot", "-Tsvg", "-O", os.devnull
-		])
+      graph.render("long_chain")
+    """
+    subprocess.check_call([
+      "dot", "-Tsvg", "-O", os.devnull
+    ])
