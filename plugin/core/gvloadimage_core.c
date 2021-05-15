3891484f1182b7ac6235bc88bccc8f9deb001315
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-05-14 17:42:05 -0700

    squash another -Wsign-conversion warning in core_loadimage_ps
    us->datasize and statbuf.st_size are equal at this point, so we may as well use
    the correctly typed one of them.

--- plugin/core/gvloadimage_core.c
+++ plugin/core/gvloadimage_core.c
@@ -181,7 +181,7 @@ static void core_loadimage_ps(GVJ_t * job, usershape_t *us, boxf b, boolean fill
 		fstat(fd, &statbuf);
 		us->datasize = (size_t)statbuf.st_size;
 #ifdef HAVE_SYS_MMAN_H
-		us->data = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+		us->data = mmap(0, us->datasize, PROT_READ, MAP_SHARED, fd, 0);
 		if (us->data == MAP_FAILED)
 			us->data = NULL;
 #else
