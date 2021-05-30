cb4479234d5208ba4fe1300ec925ee5e0025f88e
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-05-30 10:01:04 -0700

    remove unnecessary prototype
    The definition of dpd_ccw appears before any of its uses, so having a leading
    prototype is unnecessary.

--- lib/pathplan/triang.c
+++ lib/pathplan/triang.c
@@ -19,7 +19,6 @@
 #define ISCW  2
 #define ISON  3
 
-static int dpd_ccw(Ppoint_t *, Ppoint_t *, Ppoint_t *);
 static bool dpd_isdiagonal(int, int, Ppoint_t **, int);
 static bool dpd_intersects(Ppoint_t *, Ppoint_t *, Ppoint_t *, Ppoint_t *);
 static bool dpd_between(Ppoint_t *, Ppoint_t *, Ppoint_t *);
