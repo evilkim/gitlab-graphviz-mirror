e6e2b27105e675485e2b347d511d4f76f159d2b0
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-06-06 11:18:56 -0700

    add an out-of-source build to CI
    The existing CI tasks run an uncommon work flow: building distributable packages
    on each operating system. A more common work flow end users will be following
    with the portable source tarball is simply building and installing it. This
    change adds a job that does this, to provide a safeguard against breaking this
    work flow in future.
    
    The added job runs on Ubuntu 21.04. The intent is to bump this job to the latest
    each time there is a new Ubuntu release. There is no companion test job for the
    build job because we assume the existing test jobs are providing sufficient
    coverage.
    
    Closes #2073.

---
+++ ci/out-of-source-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# this script does something close to the work flow end users may follow when
+# building Graphviz
+
+set -x
+set -e
+set -o pipefail
+
+# output some info for debugging
+uname -rms
+cat /etc/os-release
+
+GV_VERSION=$(cat VERSION)
+
+# unpack the portable source tarball
+tar xfz graphviz-${GV_VERSION}.tar.gz
+
+# setup a directory for building in
+mkdir build
+cd build
+../graphviz-${GV_VERSION}/configure
+
+# build Graphviz
+make
+
+# install Graphviz
+make install
