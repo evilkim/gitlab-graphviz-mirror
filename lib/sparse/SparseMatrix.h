ellson/MOTHBALLED-graphviz

commit 508f7e82873289b7d1614dfbdad521111f0809dc

Initial update of sparse matrix library

lib/sparse/SparseMatrix.h
@@ -18,9 +18,10 @@
 
 #define SYMMETRY_EPSILON 0.0000001
 enum {FORMAT_CSC, FORMAT_CSR, FORMAT_COORD};
-enum {UNMASKED = -10};
+enum {UNMASKED = -10, MASKED = 1};
 enum {MATRIX_PATTERN_SYMMETRIC = 1<<0, MATRIX_SYMMETRIC = 1<<1, MATRIX_SKEW = 1<<2, MATRIX_HERMITIAN = 1<<3, MATRIX_UNDIRECTED = 1<<4};
 enum {BIPARTITE_RECT = 0, BIPARTITE_PATTERN_UNSYM, BIPARTITE_UNSYM, BIPARTITE_ALWAYS};
+
 
 struct SparseMatrix_struct {
   int m; /* row dimension */
@@ -33,19 +34,25 @@ struct SparseMatrix_struct {
   void *a; /* entry values. If NULL, pattern matrix */
   int format;/* whether it is CSR, CSC, COORD. By default it is in CSR format */
   int property; /* pattern_symmetric/symmetric/skew/hermitian*/
+  int size;/* size of each entry. This allows for general matrix where each entry is, say, a matrix itself */
 };
 
 typedef struct SparseMatrix_struct* SparseMatrix;
 
 enum {MATRIX_TYPE_REAL = 1<<0, MATRIX_TYPE_COMPLEX = 1<<1, MATRIX_TYPE_INTEGER = 1<<2, MATRIX_TYPE_PATTERN = 1<<3, MATRIX_TYPE_UNKNOWN = 1<<4};
 
+/* SparseMatrix_general is more general and allow elements to be 
+   any data structure, not just real/int/complex etc */
 SparseMatrix SparseMatrix_new(int m, int n, int nz, int type, int format);
+SparseMatrix SparseMatrix_general_new(int m, int n, int nz, int type, size_t sz, int format);
 
+/* this version sum repeated entries */
 SparseMatrix SparseMatrix_from_coordinate_format(SparseMatrix A);
+/* what_to_sum is SUM_REPEATED_NONE, SUM_REPEATED_ALL, SUM_REPEATED_REAL_PART, SUM_REPEATED_IMAGINARY_PART*/
 SparseMatrix SparseMatrix_from_coordinate_format_not_compacted(SparseMatrix A, int what_to_sum);
 
-SparseMatrix SparseMatrix_from_coordinate_arrays(int nz, int m, int n, int *irn, int *jcn, void *val, int type);
-SparseMatrix SparseMatrix_from_coordinate_arrays_not_compacted(int nz, int m, int n, int *irn, int *jcn, void *val, int type, int what_to_sum);
+SparseMatrix SparseMatrix_from_coordinate_arrays(int nz, int m, int n, int *irn, int *jcn, void *val, int type, size_t sz);
+SparseMatrix SparseMatrix_from_coordinate_arrays_not_compacted(int nz, int m, int n, int *irn, int *jcn, void *val, int type, size_t sz, int what_to_sum);
 
 
 void SparseMatrix_print(char *, SparseMatrix A);/*print to stdout in Mathematica format*/
@@ -96,6 +103,7 @@ real SparseMatrix_pseudo_diameter_only(SparseMatrix A);
 real SparseMatrix_pseudo_diameter_weighted(SparseMatrix A0, int root, int aggressive, int *end1, int *end2, int *connectedQ); /* assume real distances, unsymmetric matrix ill be symmetrized */
 real SparseMatrix_pseudo_diameter_unweighted(SparseMatrix A0, int root, int aggressive, int *end1, int *end2, int *connectedQ); /* assume unit edge length, unsymmetric matrix ill be symmetrized */
 void SparseMatrix_level_sets(SparseMatrix A, int root, int *nlevel, int **levelset_ptr, int **levelset, int **mask, int reintialize_mask);
+void SparseMatrix_level_sets_khops(int khops, SparseMatrix A, int root, int *nlevel, int **levelset_ptr, int **levelset, int **mask, int reintialize_mask);
 void SparseMatrix_weakly_connected_components(SparseMatrix A0, int *ncomp, int **comps, int **comps_ptr);
 void SparseMatrix_decompose_to_supervariables(SparseMatrix A, int *ncluster, int **cluster, int **clusterp);
 SparseMatrix SparseMatrix_get_submatrix(SparseMatrix A, int nrow, int ncol, int *rindices, int *cindices);
@@ -120,6 +128,29 @@ SparseMatrix SparseMatrix_sort(SparseMatrix A);
 SparseMatrix SparseMatrix_set_entries_to_real_one(SparseMatrix A);
 
 SparseMatrix SparseMatrix_complement(SparseMatrix A, int undirected);
+
+int SparseMatrix_k_centers(SparseMatrix D, int weighted, int K, int root, 
+			  int **centers, int centering, real **dist);
+
+int SparseMatrix_k_centers_user(SparseMatrix D, int weighted, int K, 
+				int *centers_user, int centering, real **dist);
+
+SparseMatrix SparseMatrix_distance_matrix_k_centers(int K, SparseMatrix D, int weighted);
+
+int SparseMatrix_distance_matrix(SparseMatrix A, int weighted,  real **dist_matrix);
+SparseMatrix SparseMatrix_distance_matrix_khops(int khops, SparseMatrix A, int weighted);
+SparseMatrix SparseMatrix_distance_matrix_k_centers(int K, SparseMatrix D, int weighted);
+
+void SparseMatrix_kcoreness(SparseMatrix A, int **coreness);/* assign coreness to each node */
+void SparseMatrix_kcore_decomposition(SparseMatrix A, int *coreness_max0, int **coreness_ptr0, int **coreness_list0);/* return the decomposition */
+
+void SparseMatrix_khairness(SparseMatrix A, int **hairness);/* assign hairness to each node */
+void SparseMatrix_khair_decomposition(SparseMatrix A, int *hairness_max0, int **hairness_ptr0, int **hairness_list0);/* return the decomposition */
+
+SparseMatrix SparseMatrix_from_dense(int m, int n, real *x);
+
+void SparseMatrix_page_rank(SparseMatrix A, real teleport_probablity, int weighted, real epsilon, real **page_rank);
+
 
 #define SparseMatrix_set_undirected(A) set_flag((A)->property, MATRIX_UNDIRECTED)
 #define SparseMatrix_set_symmetric(A) set_flag((A)->property, MATRIX_SYMMETRIC)

cmd/gvmap/DotIO.c
@@ -82,6 +82,7 @@ SparseMatrix_import_dot (Agraph_t* g, int dim, real **label_sizes, real **x, int
   real *val, *valD = NULL;
   real v;
   int type = MATRIX_TYPE_REAL;
+  size_t sz = sizeof(real);
   real padding = 10;
   int nedge_nodes = 0;
 
@@ -199,10 +200,10 @@ SparseMatrix_import_dot (Agraph_t* g, int dim, real **label_sizes, real **x, int
     }
   }
 
-  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type);
+  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type, sz);
   if (edge_label_nodes) *n_edge_label_nodes = nedge_nodes;
 
-  if (D) *D = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, valD, type);
+  if (D) *D = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, valD, type, sz);
 
   FREE(I);
   FREE(J);
@@ -567,6 +568,7 @@ SparseMatrix Import_coord_clusters_from_dot(Agraph_t* g, int maxcluster, int dim
   real* val;
   real v;
   int type = MATRIX_TYPE_REAL;
+  size_t sz = sizeof(real);
   char scluster[100];
   float ff;
   real* areap = NULL;
@@ -644,7 +646,7 @@ SparseMatrix Import_coord_clusters_from_dot(Agraph_t* g, int maxcluster, int dim
       i++;
     }
   }
-  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type);
+  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type, sz);
 
   /* get clustering info */
   *clusters = MALLOC(sizeof(int)*nnodes);
@@ -846,6 +848,7 @@ void attached_clustering(Agraph_t* g, int maxcluster, int clustering_scheme){
   real* val;
   real v;
   int type = MATRIX_TYPE_REAL;
+  size_t sz = sizeof(real);
   char scluster[100];
 
 
@@ -886,7 +889,7 @@ void attached_clustering(Agraph_t* g, int maxcluster, int clustering_scheme){
       i++;
     }
   }
-  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type);
+  A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, val, type, sz);
 
   clusters = MALLOC(sizeof(int)*nnodes);
 

cmd/tools/matrix_market.c
@@ -272,7 +272,7 @@ SparseMatrix SparseMatrix_import_matrix_market(FILE * f, int format)
 
 	if (format == FORMAT_CSR) {
 	    A = SparseMatrix_from_coordinate_arrays(nz, m, n, I, J, vp,
-						    type);
+						    type, sizeof(real));
 	} else {
 	    A = SparseMatrix_new(m, n, 1, type, FORMAT_COORD);
 	    A = SparseMatrix_coordinate_form_add_entries(A, nz, I, J, vp);

lib/neatogen/adjust.c
@@ -765,9 +765,9 @@ SparseMatrix makeMatrix(Agraph_t* g, int dim, SparseMatrix *D)
     }
 
     A = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J,
-					    val, type);
+					    val, type, sizeof(real));
 
-    if (D) *D = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, valD, type);
+    if (D) *D = SparseMatrix_from_coordinate_arrays(nedges, nnodes, nnodes, I, J, valD, type, sizeof(real));
 
     free(I);
     free(J);

lib/sfdpgen/Multilevel.c
@@ -1043,7 +1043,7 @@ static void Multilevel_coarsen_internal(SparseMatrix A, SparseMatrix *cA, Sparse
      }
     }
     assert(nzc == n);
-    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL);
+    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL, sizeof(real));
     *R = SparseMatrix_transpose(*P);
 
     *cD = DistanceMatrix_restrict_cluster(ncluster, clusterp, cluster, *P, *R, D);
@@ -1107,7 +1107,7 @@ static void Multilevel_coarsen_internal(SparseMatrix A, SparseMatrix *cA, Sparse
     }
     assert(nc == nmatch);
     assert(nzc == n);
-    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL);
+    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL, sizeof(real));
     *R = SparseMatrix_transpose(*P);
     *cA = SparseMatrix_multiply3(*R, A, *P); 
     /*
@@ -1172,7 +1172,7 @@ static void Multilevel_coarsen_internal(SparseMatrix A, SparseMatrix *cA, Sparse
       }
     }
 
-    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL);
+    *P = SparseMatrix_from_coordinate_arrays(nzc, n, nc, irn, jcn, (void *) val, MATRIX_TYPE_REAL, sizeof(real));
     *R = SparseMatrix_transpose(*P);
     *cA = SparseMatrix_multiply3(*R, A, *P); 
     if (!*cA) goto RETURN;

lib/sfdpgen/post_process.c
@@ -487,7 +487,7 @@ static void get_edge_label_matrix(relative_position_constraints data, int m, int
 	}
       }
     }
-    Lc = SparseMatrix_from_coordinate_arrays(nz, m, m, irn, jcn, val, MATRIX_TYPE_REAL);
+    Lc = SparseMatrix_from_coordinate_arrays(nz, m, m, irn, jcn, val, MATRIX_TYPE_REAL, sizeof(real));
   } else if (edge_labeling_scheme == ELSCHEME_PENALTY2 || edge_labeling_scheme == ELSCHEME_STRAIGHTLINE_PENALTY2){
     /* for an node with two neighbors j--i--k, and assume i needs to be between the old position of j and k, then the contribution to P is
        1/d_jk, and to the right hand side: {0,...,average_position_of_i's neighbor if i is an edge node,...}
@@ -517,7 +517,7 @@ static void get_edge_label_matrix(relative_position_constraints data, int m, int
 	x00[ii*dim+l] *= constr_penalty/(dist)/(ia[ii+1] - ia[ii]);
       }
     }
-    Lc = SparseMatrix_from_coordinate_arrays(nz, m, m, irn, jcn, val, MATRIX_TYPE_REAL);
+    Lc = SparseMatrix_from_coordinate_arrays(nz, m, m, irn, jcn, val, MATRIX_TYPE_REAL, sizeof(real));
     
   }
   *LL = Lc;

lib/sfdpgen/spring_electrical.c
@@ -1991,7 +1991,7 @@ static SparseMatrix shorting_edge_label_nodes(SparseMatrix A, int n_edge_label_n
     }
   }
 
-  B = SparseMatrix_from_coordinate_arrays(nz, id, id, irn, jcn, NULL, MATRIX_TYPE_PATTERN);
+  B = SparseMatrix_from_coordinate_arrays(nz, id, id, irn, jcn, NULL, MATRIX_TYPE_PATTERN, sizeof(real));
 
   FREE(irn);
   FREE(jcn);

evilkim/gitlab-graphviz-mirror

commit 9729200b7885d0bfbf5f16ca645db1c0241cfd46

remove duplicate SparseMatrix_distance_matrix_k_centers prototype
508f7e82873289b7d1614dfbdad521111f0809dc mistakenly added two duplicates of the
same prototype.

lib/sparse/SparseMatrix.h
@@ -118,8 +118,6 @@ int SparseMatrix_k_centers(SparseMatrix D, int weighted, int K, int root,
 int SparseMatrix_k_centers_user(SparseMatrix D, int weighted, int K, 
 				int *centers_user, int centering, real **dist);
 
-SparseMatrix SparseMatrix_distance_matrix_k_centers(int K, SparseMatrix D, int weighted);
-
 int SparseMatrix_distance_matrix(SparseMatrix A, int weighted,  real **dist_matrix);
 SparseMatrix SparseMatrix_distance_matrix_khops(int khops, SparseMatrix A, int weighted);
 SparseMatrix SparseMatrix_distance_matrix_k_centers(int K, SparseMatrix D, int weighted);