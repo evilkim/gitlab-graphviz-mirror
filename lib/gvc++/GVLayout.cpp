a783ab39701b9cea63f91148e6dd393c83cba7c5
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-07-07 15:55:08 +0200

    add GVLayout construction overload, passing the context as an rvalue ref

--- lib/gvc++/GVLayout.cpp
+++ lib/gvc++/GVLayout.cpp
@@ -34,6 +34,10 @@ GVLayout::GVLayout(std::shared_ptr<GVContext> gvc, CGraph::AGraph &&g,
                    const std::string &engine)
     : GVLayout(gvc, std::make_shared<CGraph::AGraph>(std::move(g)), engine) {}
 
+GVLayout::GVLayout(GVContext &&gvc, std::shared_ptr<CGraph::AGraph> g,
+                   const std::string &engine)
+    : GVLayout(std::make_shared<GVContext>(std::move(gvc)), g, engine) {}
+
 GVLayout::~GVLayout() {
   // m_gvc and m_g always either both point to an object or both are null
   assert((m_gvc != nullptr && m_g != nullptr) ||
--- lib/gvc++/GVLayout.h
+++ lib/gvc++/GVLayout.h
@@ -29,6 +29,8 @@ public:
   GVLayout(GVContext &&gvc, CGraph::AGraph &&g, const std::string &engine);
   GVLayout(std::shared_ptr<GVContext> gvc, CGraph::AGraph &&g,
            const std::string &engine);
+  GVLayout(GVContext &&gvc, std::shared_ptr<CGraph::AGraph> g,
+           const std::string &engine);
   ~GVLayout();
 
   // default copy since we manage resources through movable types
