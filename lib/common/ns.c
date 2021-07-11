13d03a79e34094e95cde90551c6cd970df336515
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-07-10 18:07:57 -0700

    reflow some text

--- lib/common/ns.c
+++ lib/common/ns.c
@@ -794,8 +794,7 @@ static int init_graph(graph_t * g)
 	    ND_priority(n)++;
 	    ED_cutvalue(e) = 0;
 	    ED_tree_index(e) = -1;
-	    if (feasible
-		&& ND_rank(aghead(e)) - ND_rank(agtail(e)) < ED_minlen(e))
+	    if (feasible && ND_rank(aghead(e)) - ND_rank(agtail(e)) < ED_minlen(e))
 		feasible = FALSE;
 	}
 	ND_tree_in(n).list = N_NEW(i + 1, edge_t *);
@@ -1156,19 +1155,16 @@ static node_t *checkdfs(graph_t* g, node_t * n)
 	w = aghead(e);
 	if (ND_onstack(w)) {
 	    dump_graph (g);
-	    fprintf(stderr, "cycle: last edge %lx %s(%lx) %s(%lx)\n",
-		(uint64_t)e,
-	       	agnameof(n), (uint64_t)n,
-		agnameof(w), (uint64_t)w);
+	    fprintf(stderr, "cycle: last edge %lx %s(%lx) %s(%lx)\n", (uint64_t)e,
+	            agnameof(n), (uint64_t)n, agnameof(w), (uint64_t)w);
 	    return w;
 	}
 	else {
 	    if (!ND_mark(w)) {
 		x = checkdfs(g, w);
 		if (x) {
-		    fprintf(stderr,"unwind %lx %s(%lx)\n",
-			(uint64_t)e,
-			agnameof(n), (uint64_t)n);
+		    fprintf(stderr,"unwind %lx %s(%lx)\n", (uint64_t)e, agnameof(n),
+		            (uint64_t)n);
 		    if (x != n) return x;
 		    fprintf(stderr,"unwound to root\n");
 		    fflush(stderr);
