1a5386164dea959bdcd68dbc3a350f8d7ec523cb
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-07-16 06:28:45 +0200

    add test of Agraph construction throwing an exception for illegal DOT source
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2001.

--- tests/test_AGraph_construction.cpp
+++ tests/test_AGraph_construction.cpp
@@ -9,3 +9,8 @@ TEST_CASE("AGraph can be constructed from DOT source and has an underlying C "
   CGraph::AGraph g{"graph {a}"};
   REQUIRE(g.c_struct() != nullptr);
 }
+
+TEST_CASE("AGraph constructed from bad DOT source throws an exception") {
+  REQUIRE_THROWS_AS(CGraph::AGraph("THIS_SHOULD_GENERATE_A_SYNTAX_ERROR"),
+                    std::runtime_error);
+}
