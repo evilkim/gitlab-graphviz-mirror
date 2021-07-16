9cd0df3b1d7beb74b6874e40aa79cd508dc8be90
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2021-07-16 06:30:50 +0200

    add test of Agraph construction throwing an exception for empty DOT source
    Towards https://gitlab.com/graphviz/graphviz/-/issues/2001.

--- tests/test_AGraph_construction.cpp
+++ tests/test_AGraph_construction.cpp
@@ -10,6 +10,10 @@ TEST_CASE("AGraph can be constructed from DOT source and has an underlying C "
   REQUIRE(g.c_struct() != nullptr);
 }
 
+TEST_CASE("AGraph constructed from an empty string throws an exception") {
+  REQUIRE_THROWS_AS(CGraph::AGraph(""), std::runtime_error);
+}
+
 TEST_CASE("AGraph constructed from bad DOT source throws an exception") {
   REQUIRE_THROWS_AS(CGraph::AGraph("THIS_SHOULD_GENERATE_A_SYNTAX_ERROR"),
                     std::runtime_error);
