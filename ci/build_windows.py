4da930cae49d1b899f642f93383c180261ad6de1
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-08-28 08:12:57 -0700

    squash a Pylint line-too-long warning

--- ci/build_windows.py
+++ ci/build_windows.py
@@ -34,7 +34,8 @@ def main(args: List[str]) -> int: #pylint: disable=C0116
     if build.exists():
       shutil.rmtree(build)
     os.makedirs(build)
-    run(["cmake", "-G", "Visual Studio 16 2019", "-A", options.platform, "-D", "with_cxx_api=ON", ".."],
+    run(["cmake", "-G", "Visual Studio 16 2019", "-A", options.platform, "-D",
+         "with_cxx_api=ON", ".."],
         build)
     run(["cmake", "--build", ".", "--config", options.configuration], build)
     run(["cpack", "-C", options.configuration], build)
