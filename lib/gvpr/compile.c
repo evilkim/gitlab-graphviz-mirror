8dd3a198ff0168f37c816a20473571035b92e8f9
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-09-11 10:54:13 -0700

    gvpr compile: make 'sfx' parameter const
    The pointed to data is not modified within this function.

--- lib/gvpr/compile.c
+++ lib/gvpr/compile.c
@@ -2265,8 +2265,7 @@ static Exdisc_t *initDisc(Gpr_t * state)
  * typed expression.
  */
 static Exnode_t *compile(Expr_t * prog, char *src, char *input, int line,
-			 char *lbl, char *sfx, int kind)
-{
+                         char *lbl, const char *sfx, int kind) {
     Exnode_t *e = 0;
     Sfio_t *sf;
     Sfio_t *prefix;
