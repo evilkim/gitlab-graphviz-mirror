e35b2e22d931956b13ebdd5f32d738a088ee2b9e
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-09-11 11:00:05 -0700

    gvpr compileProg: remove some dead code
    The `doFlags` function, at a minimum, prints "\n" into its buffer so there is
    never a case when the result is an empty string.

--- lib/gvpr/compile.c
+++ lib/gvpr/compile.c
@@ -2449,10 +2449,6 @@ comp_prog *compileProg(parse_prog * inp, Gpr_t * state, int flags)
 
     if (flags) {
 	endg_sfx = strdup (doFlags(flags, tmps));
-	if (*endg_sfx == '\0') {
-	    free(endg_sfx);
-	    endg_sfx = 0;
-	}
     }
 
     if (!initDisc(state))
