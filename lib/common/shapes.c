13f80de90088f3395bc24347237d35e13be2896b
Matthew Fernandez <matthew.fernandez@gmail.com>
2021-09-18 15:43:17 -0700

    p_terminator: squash a -Wmissing-field-initializer warning

--- lib/common/shapes.c
+++ lib/common/shapes.c
@@ -143,7 +143,8 @@ static polygon_t p_star = {
 static polygon_t p_promoter = {
     .peripheries = 1, .sides = 4, .option = PROMOTER};
 static polygon_t p_cds = {.peripheries = 1, .sides = 4, .option = CDS};
-static polygon_t p_terminator = { FALSE, 1, 4, 0., 0., 0., TERMINATOR};
+static polygon_t p_terminator = {
+    .peripheries = 1, .sides = 4, .option = TERMINATOR};
 static polygon_t p_utr = { FALSE, 1, 4, 0., 0., 0., UTR};
 static polygon_t p_insulator = { FALSE, 1, 4, 0., 0., 0., INSULATOR};
 static polygon_t p_ribosite = { FALSE, 1, 4, 0., 0., 0., RIBOSITE};
